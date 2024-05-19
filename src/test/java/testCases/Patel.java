26de90d1217ee8cfd0ab9bcad800f5027323648a
renildk7878 <rkatrodiya7878@gmail.com>
2024-05-19 15:42:52 -0700

    Changed on Patel filese

--- src/test/java/testCases/Patel.java
+++ src/test/java/testCases/Patel.java
@@ -1,5 +1,12 @@
 package testCases;
 
+import org.testng.annotations.Test;
+
 public class Patel {
 
+	@Test
+	private void Test1() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World...");
+	}
 }
